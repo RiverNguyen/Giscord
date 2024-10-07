7c2ae4ab0274e48812e92e38e8bdaf52380bb8c6
RiverNguyen <ngiang1309@gmail.com>
2024-10-07 16:03:49 +0700

    CREATE: Channels and channel api

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,7 @@ import { siteConfig } from "@/config/site";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Open_Sans } from "next/font/google";
 
-import "./globals.css";
-
+import "@/app/globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ModalProvider } from "@/components/providers/modal-provider";
