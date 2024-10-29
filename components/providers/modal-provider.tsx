5e0b43140f2d502533ba52c059f184775959528c
RiverNguyen <ngiang1309@gmail.com>
2024-10-29 17:09:32 +0700

    CREATE: delete channel

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -7,17 +7,21 @@ import { MembersModal } from "@/components/modals/members-modal";
 import { CreateChannelModal } from "@/components/modals/create-channel-modal";
 import { LeaveServerModal } from "@/components/modals/leave-server-modal";
 import { DeleteServerModal } from "@/components/modals/delete-server-modal";
+import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
+import { EditChannelModal } from "@/components/modals/edit-channel-modal";
 
 export const ModalProvider = () => {
-  return (
-    <>
-      <CreateServerModal />
-      <InviteModal />
-      <EditServerModal />
-      <MembersModal />
-      <CreateChannelModal />
-      <LeaveServerModal />
-      <DeleteServerModal />
-    </>
-  );
+	return (
+		<>
+			<CreateServerModal/>
+			<InviteModal/>
+			<EditServerModal/>
+			<MembersModal/>
+			<CreateChannelModal/>
+			<LeaveServerModal/>
+			<DeleteServerModal/>
+			<DeleteChannelModal/>
+			<EditChannelModal/>
+		</>
+	);
 };
--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,32 +1,35 @@
 import { create } from "zustand";
-import { ChannelType, Server } from "@prisma/client";
+import { Channel, ChannelType, Server } from "@prisma/client";
 
 export type ModalType =
-  | "createServer"
-  | "invite"
-  | "editServer"
-  | "members"
-  | "createChannel"
-  | "leaveServer"
-  | "deleteServer";
+	| "createServer"
+	| "invite"
+	| "editServer"
+	| "members"
+	| "createChannel"
+	| "leaveServer"
+	| "deleteServer"
+	| "deleteChannel"
+	| "editChannel";
 
 interface ModalData {
-  server?: Server;
-  channelType?: ChannelType;
+	server?: Server;
+	channel?: Channel;
+	channelType?: ChannelType;
 }
 
 interface ModalStore {
-  type: ModalType | null;
-  data: ModalData;
-  isOpen: boolean;
-  onOpen: (type: ModalType, data?: ModalData) => void;
-  onClose: () => void;
+	type: ModalType | null;
+	data: ModalData;
+	isOpen: boolean;
+	onOpen: (type: ModalType, data?: ModalData) => void;
+	onClose: () => void;
 }
 
 export const useModal = create<ModalStore>((set) => ({
-  type: null,
-  isOpen: false,
-  data: {},
-  onOpen: (type, data = {}) => set({ isOpen: true, type, data }),
-  onClose: () => set({ type: null, isOpen: false }),
+	type: null,
+	isOpen: false,
+	data: {},
+	onOpen: (type, data = {}) => set({ isOpen: true, type, data }),
+	onClose: () => set({ type: null, isOpen: false }),
 }));
