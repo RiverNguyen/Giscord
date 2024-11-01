061bd73c9b236dd32c139826b205f2e08c80cf7b
RiverNguyen <ngiang1309@gmail.com>
2024-11-01 15:54:26 +0700

    CREATE: Channel Page and Conversation Page

--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -17,17 +17,24 @@ const roleIconMap = {
   [MemberRole.MODERATOR]: (
     <ShieldCheck className="mr-2 w-4 h-4 text-indigo-500" />
   ),
-  [MemberRole.ADMIN]: <ShieldAlert className="mr-2 w-4 h-4 text-rose-500" />,
+  [MemberRole.ADMIN]: (
+    <ShieldAlert className=" w-4 h-4 ml-auto text-rose-500" />
+  ),
 };
 
 export const ServerMember = ({ member, server }: ServerMemberProps) => {
   const params = useParams();
   const router = useRouter();
 
+  const onClick = () => {
+    router.push(`/servers/${server?.id}/conversations/${member.id}`);
+  };
+
   const icon = roleIconMap[member.role];
 
   return (
     <button
+      onClick={onClick}
       className={cn(
         `group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1`,
         params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700"
@@ -39,7 +46,7 @@ export const ServerMember = ({ member, server }: ServerMemberProps) => {
       />
       <p
         className={cn(
-          `font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition`,
+          `font-semibold text-xs text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition`,
           params?.memberId === member.id &&
             "text-primary dark:text-zinc-200 dark:group-hover:text-white"
         )}
