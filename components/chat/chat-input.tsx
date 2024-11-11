e3a385fbb7bda5a55bbfe6e7c9ac4d950a581721
RiverNguyen <ngiang1309@gmail.com>
2024-11-11 21:15:36 +0700

    UPDATE: API Message

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -31,14 +31,14 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
 
   const isLoading = form.formState.isSubmitting;
 
-  const onSubmit = async (value: z.infer<typeof formSchema>) => {
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
       const url = qs.stringifyUrl({
         url: apiUrl,
         query,
       });
 
-      await axios.post(url, value);
+      await axios.post(url, values);
     } catch (error) {
       console.log("[CONVERSATION_INPUT]", error);
     }
